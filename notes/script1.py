7d4b28fc8d220e8eaa3c08a7bf335f4d22a3e919
Tochukwu Ofodum <championpraise16@gmail.com>
2025-08-20 17:06:31 +0100

    Add initial Python script with basic functionality

---
+++ notes/script1.py
@@ -0,0 +1,6 @@
+# A first python script
+import sys         # Load a library module
+print(sys.platform)
+print(2 ** 100)    # Raise 2 to the power of 100
+x = 'Hack!'
+print(x * 8)       # String Repetition
